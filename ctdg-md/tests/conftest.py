956317cc30e445798835a9eeedd971ee4768c793
Siddhant Khatod <kishanson73@gmail.com>
2026-09-18 15:28:31 +0530

    Delete ctdg-md/tests directory

--- ctdg-md/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Importing the API in tests must never silently use an arbitrary user checkpoint.
-os.environ.pop("CTDG_CHECKPOINT", None)
-os.environ.pop("CTDG_ALLOW_UNTRAINED", None)
